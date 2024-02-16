670985fa75c0b49c17c6a94f781976ba56d28f57
Gustavson3D <gustavodrbacca@gmail.com>
2024-02-16 16:31:44 -0300

    Categorias adicionadas.

--- Teste/explore/smarttravel/admin.py
+++ Teste/explore/smarttravel/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Categorias
 
-# Register your models here.
+@admin.register(Categorias)
+class CategoriasAdmin(admin.ModelAdmin):
+    # Personalize o comportamento do admin, se necessário
+    list_display = ('id_categorias', 'categorias')
+    search_fields = ['categorias']
+
