4c3e526728205c89fa1b14cb2eddeaf54edafd59
Gustavson3D <gustavodrbacca@gmail.com>
2024-02-21 21:57:32 -0300

    clone explore deletado.

--- explore/cart/context.processor.py
+++
@@ -1,6 +0,0 @@
-from.cart import Cart
-
-# criando um processasdor de contexto para o  carrinho de compras funcionar em qualquer lugar do projeto
-def cart(request):
-    # retornar os dados padrão do Cart
-    return {'cart': Cart(request)}
