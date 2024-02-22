0c5445997980efa8c7a4df375ccdf87f971382f2
Gustavson3D <gustavodrbacca@gmail.com>
2024-02-22 10:32:57 -0300

    Função para adicionar ao carrinho "completa"

---
+++ explore/cart/cart.py
@@ -0,0 +1,27 @@
+class Cart():
+    def __init__ (self, request):
+        self.session = request.session
+
+        # pegar a key da sessão casso a sessão exista
+        cart = self.session.get('session_key')
+
+        # se o usuário for novo, sem key, crie uma sessão
+        if "session_key" not in request.session:
+            cart = self.session['session_key'] = {}
+
+        self.cart = cart
+
+    def __len__(self):
+        return len(self.cart)
+    
+
+    def add(self, product):
+
+        product_id = product.id
+
+        if product_id in self.cart:
+            pass
+        else:
+            self.cart[product_id] = {'Nome': product.nome_local}
+        
+        self.session.modified = True
---
+++ explore/cart/views.py
@@ -0,0 +1,55 @@
+import json
+from django.shortcuts import render, get_object_or_404
+from .cart import Cart
+from smarttravel.models import Local
+from django.http import JsonResponse
+from django.http import HttpResponse
+
+
+def cart_summary(request):
+    return render(request, 'cart_summary.html', {})
+
+
+# def cart_add(request):
+#     # puxar o carrinho
+#     cart = Cart(request)
+#     #Testar para postar
+#     if request.POST.get('action') == 'post':
+#         #return('O que veio no POST:')
+#         product_id = request.POST.get('product_id') # Alteração aqui
+#         # procurar o produto na database
+#         product = get_object_or_404(Local, id=product_id)
+
+#         # Salvar na sessão
+#         cart.add(product=product)
+
+#         response = JsonResponse({'Local Nome': product.nome_local})  # Use 'nome_local' em vez de 'name'
+#         return response
+    
+#     #return  HttpResponse("Not a valid request")
+
+def cart_add(request):
+    # puxar o carrinho
+    cart = Cart(request)
+    
+    # Testar para postar
+    if request.POST.get('action') == 'post':
+        product_id = request.POST.get('product_id')  # Alteração aqui
+        print(product_id)
+        # procurar o produto na database
+        product = get_object_or_404(Local, id=product_id)
+        # Salvar na sessão
+        cart.add(product=product)
+
+        response = JsonResponse({'Local Nome': product.nome_local})  
+        return response
+    
+    # Retorne uma resposta de erro se não for uma solicitação POST válida
+    return HttpResponse("Not a valid request")
+
+def cart_delete(request):
+    pass
+
+
+def cart_update(request):
+    pass
