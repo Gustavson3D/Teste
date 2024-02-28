0f86f73ea7e572b5b5b34ae8a4ac3329aead96c7
Gustavson3D <gustavodrbacca@gmail.com>
2024-02-28 08:48:07 -0300

    email delete.

--- explore/envia_email/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.core.mail import send_mail
-
-# Create your views here.
-
-def envia_email(request):
-    send_mail('Assunto', 'Email')
-    return HttpResponse('ola')
